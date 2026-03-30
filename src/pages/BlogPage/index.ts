2511441d869db973d660dc99042e48b761641076
Papernov Alexander <impudev@gmail.com>
2026-03-30 22:19:39 +1000

    changed min_resolution for mini phones bug fix with side_panel items button colors

--- src/pages/BlogPage/index.ts
+++ src/pages/BlogPage/index.ts
@@ -1,3 +1,3 @@
-import { BlogPageAsync } from './ui/BlockPage.async'
+import { BlogPageAsync } from './ui/BlogPage.async'
 
 export { BlogPageAsync as BlogPage }
--- src/pages/EmployerPage/ui/EmployerPage.tsx
+++ src/pages/EmployerPage/ui/EmployerPage.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
+import { Container } from '@/shared/ui/Container'
+import { Page } from '@/widgets/Page'
 
 const EmployerPage = () => {
 	const { t } = useTranslation('employer_page')
-	return <div>{t('Employer Page')}</div>
+	return (
+		<Page>
+			<div className="primary_container">
+				<Container>{t('Employer Page')}</Container>
+			</div>
+		</Page>
+	)
 }
 
 export default EmployerPage
